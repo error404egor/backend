3b511785f7c849355acfde1471bdc2bdcca7c161
Mamlik <57287362+Mamlik@users.noreply.github.com>
2023-02-18 23:30:52 +0300

    registartion-authorization-added-v-1-commented

--- data/user.py
+++ data/user.py
@@ -3,16 +3,16 @@ from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
-    __tablename__ = 'user'
+class User(SqlAlchemyBase):  # таблица пользователей базы данных
+    __tablename__ = 'user'  # название таблицы
     id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True, unique=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=False)
-    mail = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=True)
-    password = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=False)
+                           primary_key=True, autoincrement=True, unique=True)  # id пользователя
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=False)  # имя пользователя
+    mail = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=True)  # почта пользователя
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="", unique=False)  # пароль пользователя
 
-    def set_password(self, password):
+    def set_password(self, password):  # функция для хеширования пароля
         self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password):  # функция для проверки пароля
         return check_password_hash(self.hashed_password, password)
